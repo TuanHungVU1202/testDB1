059546d5998ecea842af89250e9e7ad98be23196
TuanHungVU1202 <41147045+TuanHungVU1202@users.noreply.github.com>
2018-09-25 00:09:28 +0700

    done django doc tut #3

--- testConnection/views.py
+++ testConnection/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+#from django.template import loader
+
+
+#import from models
 from .models import Question
 
 # Create your views here.
 def index(request):
-	lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([q.question_text for q in lastest_question_list])
-	return HttpResponse(output)
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	
+	'''import loader to use the following
+	template = loader.get_template('testConnection/index.html')
+	'''
+
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	#return HttpResponse(template.render(context, request))
+	return render(request, 'testConnection/index.html', context)
 
 
 def detail(request, question_id):
-	return HttpResponse("This is the question %s." %question_id)
+	''' Not using get_object_or_404 shortcut
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404('Question not exist')
+	return render(request, 'testConnection/detail.html', {'question': question})
+	'''
+	#return HttpResponse("This is the question %s." %question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'testConnection/detail.html', {'question': question})
 
 
 def results(request, question_id):
@@ -20,3 +44,5 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("Voting on question %s" %question_id)
+
+
