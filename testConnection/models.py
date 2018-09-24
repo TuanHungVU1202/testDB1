ab6435e2df75cbf2e756abb964d1e467086dac72
TuanHungVU1202 <41147045+TuanHungVU1202@users.noreply.github.com>
2018-09-24 22:45:03 +0700

    changed templates name

--- testConnection/models.py
+++ testConnection/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+
+import datetime
 
 # Create your models here.
+'''
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+	date = models.DateTimeField(auto_now_add=True)
+'''
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('publised date')
+	def __str__(self):
+		return self.question_text
+	def wasPublishedRecently(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete = models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+	def __str__(self):
+		return self.choice_text
+
